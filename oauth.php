33bc8c408cb5f97f1a3c7d82fc1bee2bbd0a9e1a
kbrisbin <kbrisbin@google.com>
2011-04-08 01:27:58 +0000

    updates to library

--- oauth.php
+++ oauth.php
@@ -3,12 +3,7 @@
 include_once "oauth-php/library/OAuthStore.php";
 include_once "oauth-php/library/OAuthRequester.php";
 
-define('SCOPE', 'http://www.google.com/fusiontables/api/query');
-define('SERVER_URI', 'https://www.google.com');
-define('GOOGLE_OAUTH_REQUEST_TOKEN_API', 'https://www.google.com/accounts/OAuthGetRequestToken');
-define('GOOGLE_OAUTH_ACCESS_TOKEN_API', 'https://www.google.com/accounts/OAuthGetAccessToken');
-define('GOOGLE_OAUTH_AUTHORIZE_API', 'https://www.google.com/accounts/OAuthAuthorizeToken');
-
+include_once "constants.php";
 
 class OAuthClient {
   
