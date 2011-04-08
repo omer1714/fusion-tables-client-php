d7f5cf37e01dbfdae91bc41c97ccadc4c98e7b62
kbrisbin@google.com <kbrisbin@google.com@9d8282ac-b56a-1fa4-b598-79394d612efa>
2011-04-08 01:27:58 +0000

    updates to library

--- trunk/oauth_ext.php
+++ trunk/oauth_ext.php
@@ -1,11 +1,6 @@
 <?php
 
-define('SCOPE', 'http://www.google.com/fusiontables/api/query');
-define('SERVER_URI', 'https://www.google.com');
-define('GOOGLE_OAUTH_REQUEST_TOKEN_API', 'https://www.google.com/accounts/OAuthGetRequestToken');
-define('GOOGLE_OAUTH_ACCESS_TOKEN_API', 'https://www.google.com/accounts/OAuthGetAccessToken');
-define('GOOGLE_OAUTH_AUTHORIZE_API', 'https://www.google.com/accounts/OAuthAuthorizeToken');
-
+include_once "constants.php";
 
 class OAuthClientExt {
   
